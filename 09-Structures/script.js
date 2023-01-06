3deaa165e1fef7d7d5b561d7bd8b8d2c76fb4b7b
nguyenquochuy12345 <quochuynguyen11112003@gmail.com>
2023-01-07 00:52:50 +0700

    103. Destructuring Arrays

---
+++ 09-Structures/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Breschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    
+    order: function(starterIndex, mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+};
+
+const arr = [2,3,4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+// Chuyen doi cac bien
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main,secondary);
+
+// Cách nhần hai giá trị về một hàm
+const [starter , mainCourse]  = (restaurant.order(2, 0));
+console.log(starter , mainCourse);
+
+// Cách lấy giá trị của mảng lồng nhau
+const nested = [2,4,[5,6]];
+const [i, ,[j,k]] = nested;
+console.log(i,j,k);   
+
+// Default value
+const [p = 1, q = 1, r = 1] = [8, 9]
+console.log(p, q, r);
+
+
