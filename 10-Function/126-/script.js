0e7d2fe7eb73fe05ad67d3595c8b7b960d402e2c
nguyenquochuy12345 <quochuynguyen11112003@gmail.com>
2023-02-09 00:41:48 +0700

    131. Functions Accepting Callback Functions

--- 10-Function/126-/script.js
+++ 10-Function/126-/script.js
@@ -24,37 +24,69 @@
 
 
 
-// ---------------129. Default Parameters-------------
-const flight = 'LH234';
-const jonas = {
-    name: 'Jonas Schop',
-    passport: 132465798,
+// // ---------------129. Default Parameters-------------
+// const flight = 'LH234';
+// const jonas = {
+//     name: 'Jonas Schop',
+//     passport: 132465798,
+// };
+
+// const checkIn = function(flightNum, passenger){
+//     flightNum = "LH999";
+//     passenger.name = 'MR. ' + passenger.name;
+
+//     if(passenger.passport === 132465798){
+//         alert('checked in');
+//     }else{
+//         alert('Wrong passport!')
+//     }
+// }
+
+// checkIn(flight,jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // // is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//     person.passport = Math.trunc(Math.random() * 100000000);
+// };
+
+// newPassport(jonas);
+
+// checkIn(flight ,jonas);
+// console.log(jonas)
+
+
+
+
+// // ---------------129. Default Parameters-------------
+
+const oneWord = function(str){
+    return str.replace(/ /g, '').toLowerCase();
 };
 
-const checkIn = function(flightNum, passenger){
-    flightNum = "LH999";
-    passenger.name = 'MR. ' + passenger.name;
-
-    if(passenger.passport === 132465798){
-        alert('checked in');
-    }else{
-        alert('Wrong passport!')
-    }
-}
-
-checkIn(flight,jonas);
-console.log(flight);
-console.log(jonas);
-
-// // is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
-
-const newPassport = function (person) {
-    person.passport = Math.trunc(Math.random() * 100000000);
+const upperFirstWord = function (str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
 };
 
-newPassport(jonas);
+// Higher-order function
+const transformer = function (str , fn) {
+    console.log(`Original string : ${str}`);
+    console.log(`Transformed string : ${fn(str)}`);
+    
+    console.log(`Transformed by: ${fn.name}`);
+};
 
-checkIn(flight ,jonas);
-console.log(jonas)
+transformer('JavaScript is the  best !', upperFirstWord);
+
+// JS uses callbacks all the time 
+const high5 = function () {
+    console.log('hello');
+};
+
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha' , 'Adam'].forEach(high5);
