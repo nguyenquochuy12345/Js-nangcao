855c6287b2079eebb79868a17dc337abab820917
nguyenquochuy12345 <quochuynguyen11112003@gmail.com>
2023-02-10 20:28:45 +0700

    143. The new at Method

--- 11-with_arrays/javascript.js
+++ 11-with_arrays/javascript.js
@@ -63,37 +63,51 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-//-------------- 142. Simple Array Methods ----------------------
-//  SLICE : cắt chuỗi mảng
-//  SPLICE : cắt và xóa mảng ban đầu
-//  REVERSE : đảo lại mảng
-//  CONCAT : nối mảng
-//  JOIN :  Trả về chuỗi từ các dữ liệu trong mảng
+// /////////////////////////////////////////////////
+// //-------------- 142. Simple Array Methods ----------------------
+// //  SLICE : cắt chuỗi mảng
+// //  SPLICE : cắt và xóa mảng ban đầu
+// //  REVERSE : đảo lại mảng
+// //  CONCAT : nối mảng
+// //  JOIN :  Trả về chuỗi từ các dữ liệu trong mảng
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-// SLICE
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
-// SPLICE
-// console.log(arr.splice(2));
-arr.splice(-1);
-console.log(arr);
-arr.splice(1, 2);
-console.log(arr);
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-// JOIN
-console.log(letters.join(' - '));
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// // SLICE
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+// console.log([...arr]);
+// // SPLICE
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+// arr.splice(1, 2);
+// console.log(arr);
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+// // JOIN
+// console.log(letters.join(' - '));
+
+
+//-------------- 143. The new at Method ----------------------
+
+// The new at Method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
